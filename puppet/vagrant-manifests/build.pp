2966afbf53d51e01dab8aa8a6ca070d036c7add8
Daniel P. McAllaster <dmcallaster@is-llc.net>
2015-06-18 16:48:02 -0700

    Adding build machine to Vagrant

---
+++ puppet/vagrant-manifests/build.pp
@@ -0,0 +1,13 @@
+# Turn off virtual packages unless overridden by Hiera:
+if versioncmp($::puppetversion,'3.6.1') >= 0 {
+  $allow_virtual_packages = hiera('allow_virtual_packages',false)
+
+  Package {
+    allow_virtual => $allow_virtual_packages,
+  }
+}
+
+# Modules to use:
+include epel
+include build-packages
+include jenkins
