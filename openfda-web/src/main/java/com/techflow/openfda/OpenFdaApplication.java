67b9e6186c1ce22284a7093052bef3709f50d93b
David Andersen <dandersen@techflow.com>
2015-06-19 10:16:20 -0700

    add comments to OpenFdaApplication

--- openfda-web/src/main/java/com/techflow/openfda/OpenFdaApplication.java
+++ openfda-web/src/main/java/com/techflow/openfda/OpenFdaApplication.java
@@ -9,25 +9,37 @@ import com.techflow.openfda.drug.client.OpenFdaGatewayImpl;
 import com.techflow.openfda.drugs.OpenFdaUseCaseFactory;
 import com.techflow.openfda.drugs.SimpleOpenFdaSpringUseCaseFactory;
 
+/**
+ * Application entry point. Run {@link OpenFdaApplication#main(String[])} to start the server.
+ */
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
 public class OpenFdaApplication
 {
-	public static void main(String[] args) throws Exception
-	{
-		SpringApplication.run(OpenFdaApplication.class, args);
-	}
-
+	/**
+	 * Create the use case factory.
+	 */
 	@Bean
 	public OpenFdaUseCaseFactory useCaseFactory(OpenFdaGateway fdaGateway)
 	{
 		return new SimpleOpenFdaSpringUseCaseFactory(fdaGateway);
 	}
 
+	/**
+	 * Create the Open FDA gateway for API calls.
+	 */
 	@Bean
-	public OpenFdaGateway fdaGateway()
+	public OpenFdaGateway openFdaGateway()
 	{
 		return new OpenFdaGatewayImpl();
+	}
+
+	/**
+	 * Starts the server on port 8080.
+	 */
+	public static void main(String[] args) throws Exception
+	{
+		SpringApplication.run(OpenFdaApplication.class, args);
 	}
 }
