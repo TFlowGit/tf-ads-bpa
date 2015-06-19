011da563c8232bb67031f22f15bd982ec378f225
hchau619 <hchau619@gmail.com>
2015-06-19 16:42:48 -0700

    add Drug not found error message and drug info visibility

--- openfda-web/src/main/resources/public/js/controllers/mainController.js
+++ openfda-web/src/main/resources/public/js/controllers/mainController.js
@@ -1,23 +1,35 @@
 
 drugflowApp.controller('mainCtrl', ['$scope', 'drugsService', function ($scope, drugsService) {
-  $scope.labelName = "LABEL NAMEs";
+
   $scope.query ='';
   $scope.result = '';
+  $scope.visibility = false;
   $scope.searchDrug = function() {
-	  drugsService.getDrugInfo($scope.query).success(function(response){
-			var result = {};
-			var labelInfo = {};
-			for( key in response ) {
-				if(key == 'name') result['name'] = response[key];
-				else if(key == 'purpose') result['purpose'] = response[key];
-				else {
-					labelInfo[key] = response[key];
-				}
-			}
-			result['labelInfo'] = labelInfo;	
-			$scope.result = result;
-			console.log(result);
+	  $scope.queryFailedMsg = '';
+	  drugsService.getDrugInfo($scope.query).
+	  	success(function(response){
+	  		$scope.visibility = true;
+			transformResponse(response);
+		}).
+		error(function(response){
+			$scope.visibility = false;
+			$scope.queryFailedMsg = "Drug not found";
 		});
   };
+  
+  var transformResponse = function(response){
+	  	var result = {};
+	  	var labelInfo = {};
+		for( key in response ) {
+			if(key == 'name') result['name'] = response[key];
+			else if(key == 'purpose') result['purpose'] = response[key];
+			else if (response[key] !== null) {
+				labelInfo[key] = response[key];
+			}
+		}
+		result['labelInfo'] = labelInfo;	
+		$scope.result = result;
+		console.log(result);
+  }
 }]);
 
