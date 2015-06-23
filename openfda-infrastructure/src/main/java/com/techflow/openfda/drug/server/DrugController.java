651c7ef2ccc45801a06745074cd68b4da855304d
David Andersen <dandersen@techflow.com>
2015-06-23 16:26:15 -0700

    document the api w/swagger

--- openfda-infrastructure/src/main/java/com/techflow/openfda/drug/server/DrugController.java
+++ openfda-infrastructure/src/main/java/com/techflow/openfda/drug/server/DrugController.java
@@ -1,17 +1,23 @@
 package com.techflow.openfda.drug.server;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import com.techflow.openfda.GatewayException;
 import com.techflow.openfda.drug.usecase.FindDrugUseCase;
 import com.techflow.openfda.drug.usecase.OpenFdaUseCaseFactory;
 
+@Api(value = "/api/drugs")
 @Controller
 @RequestMapping("/api/drugs")
 public class DrugController
@@ -24,7 +30,13 @@ public class DrugController
 		this.useCaseFactory = useCaseFactory;
 	}
 
-	@RequestMapping(value = "")
+	@ApiOperation(value = "Describe a drug")
+	@ApiResponses({
+			@ApiResponse(message = "Success", code = 200, response = DescribeDrugResponse.class),
+			@ApiResponse(message = "Drug not found", code = 404),
+			@ApiResponse(message = "Internal error", code = 500, response = ErrorResponse.class)
+	})
+	@RequestMapping(value = "", method = RequestMethod.GET)
 	@ResponseBody
 	public ResponseEntity<DescribeDrugResponse> describeDrug(DescribeDrugRequest request) throws GatewayException
 	{
