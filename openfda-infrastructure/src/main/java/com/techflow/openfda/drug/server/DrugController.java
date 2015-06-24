c627b0248200c5522e86d8db86ff04f749530b6b
David Andersen <dandersen@techflow.com>
2015-06-23 17:05:56 -0700

    improve swagger docs

--- openfda-infrastructure/src/main/java/com/techflow/openfda/drug/server/DrugController.java
+++ openfda-infrastructure/src/main/java/com/techflow/openfda/drug/server/DrugController.java
@@ -1,11 +1,14 @@
 package com.techflow.openfda.drug.server;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import springfox.documentation.annotations.ApiIgnore;
 import com.techflow.openfda.GatewayException;
 import com.techflow.openfda.drug.usecase.FindDrugUseCase;
 import com.techflow.openfda.drug.usecase.OpenFdaUseCaseFactory;
@@ -30,15 +34,17 @@ public class DrugController
 		this.useCaseFactory = useCaseFactory;
 	}
 
-	@ApiOperation(value = "Describe a drug")
+	@ApiOperation(value = "Describe a drug", produces = MediaType.APPLICATION_JSON_VALUE)
 	@ApiResponses({
 			@ApiResponse(message = "Success", code = 200, response = DescribeDrugResponse.class),
 			@ApiResponse(message = "Drug not found", code = 404),
 			@ApiResponse(message = "Internal error", code = 500, response = ErrorResponse.class)
 	})
 	@RequestMapping(value = "", method = RequestMethod.GET)
+	@ApiImplicitParams({ @ApiImplicitParam(name = "name", value = "The name of a drug", paramType = "query", required = false, dataType = "string")
+	})
 	@ResponseBody
-	public ResponseEntity<DescribeDrugResponse> describeDrug(DescribeDrugRequest request) throws GatewayException
+	public ResponseEntity<DescribeDrugResponse> describeDrug(@ApiIgnore DescribeDrugRequest request) throws GatewayException
 	{
 		final FindDrugUseCase useCase = useCaseFactory.newFindDrugUseCase();
 		final DescribeDrugResponse response = new DescribeDrugResponse();
