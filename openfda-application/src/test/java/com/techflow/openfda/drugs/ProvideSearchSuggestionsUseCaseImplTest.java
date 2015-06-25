83ad5ef4280f886e01e44307732ddf62cbcaea8d
David Andersen <dandersen@techflow.com>
2015-06-25 16:07:37 -0700

    limit search suggestions to 5

--- openfda-application/src/test/java/com/techflow/openfda/drugs/ProvideSearchSuggestionsUseCaseImplTest.java
+++ openfda-application/src/test/java/com/techflow/openfda/drugs/ProvideSearchSuggestionsUseCaseImplTest.java
@@ -2,6 +2,8 @@ package com.techflow.openfda.drugs;
 
 import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.hasSize;
 import static org.junit.Assert.assertThat;
 import java.util.List;
 import org.junit.Test;
@@ -52,6 +54,63 @@ public class ProvideSearchSuggestionsUseCaseImplTest
 		useCase.execute();
 
 		assertThat(response.getResults(), containsInAnyOrder("tylenol", "tylenol pm"));
+	}
+
+	@Test
+	public void dontSearchForBlank() throws Exception
+	{
+		final ProvideSearchSuggestionsUseCaseImpl useCase = new ProvideSearchSuggestionsUseCaseImpl(new MockDrugRepository());
+
+		useCase.setRequest(new ProvideSearchSuggestionsRequest() {
+			@Override
+			public String getDrug()
+			{
+				return "";
+			}
+		});
+		final SuggestionsResponseSpy response = new SuggestionsResponseSpy();
+		useCase.setResponse(response);
+		useCase.execute();
+
+		assertThat(response.getResults(), empty());
+	}
+
+	@Test
+	public void dontSearchForNull() throws Exception
+	{
+		final ProvideSearchSuggestionsUseCaseImpl useCase = new ProvideSearchSuggestionsUseCaseImpl(new MockDrugRepository());
+
+		useCase.setRequest(new ProvideSearchSuggestionsRequest() {
+			@Override
+			public String getDrug()
+			{
+				return null;
+			}
+		});
+		final SuggestionsResponseSpy response = new SuggestionsResponseSpy();
+		useCase.setResponse(response);
+		useCase.execute();
+
+		assertThat(response.getResults(), empty());
+	}
+
+	@Test
+	public void limitSuggestionsTo5() throws Exception
+	{
+		final ProvideSearchSuggestionsUseCaseImpl useCase = new ProvideSearchSuggestionsUseCaseImpl(new MockDrugRepository());
+
+		useCase.setRequest(new ProvideSearchSuggestionsRequest() {
+			@Override
+			public String getDrug()
+			{
+				return "x";
+			}
+		});
+		final SuggestionsResponseSpy response = new SuggestionsResponseSpy();
+		useCase.setResponse(response);
+		useCase.execute();
+
+		assertThat(response.getResults(), hasSize(5));
 	}
 
 	private final class SuggestionsResponseSpy implements ProvideSearchSuggestionsResponse
