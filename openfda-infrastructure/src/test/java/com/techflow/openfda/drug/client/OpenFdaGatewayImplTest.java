c33cb4d3829e437c1dd8febbe982ac2f64b1b0fc
Hank Ratzesberger <hratzesberger@techflow.com>
2015-06-26 10:51:36 -0700

    Updated Drug name repository - fixed file

--- openfda-infrastructure/src/test/java/com/techflow/openfda/drug/client/OpenFdaGatewayImplTest.java
+++ openfda-infrastructure/src/test/java/com/techflow/openfda/drug/client/OpenFdaGatewayImplTest.java
@@ -1,6 +1,7 @@
 package com.techflow.openfda.drug.client;
 
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.lessThanOrEqualTo;
 import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
@@ -15,25 +16,9 @@ import com.techflow.openfda.GatewayException;
 import com.techflow.openfda.drugs.DrugEventSummary;
 import com.techflow.openfda.drugs.DrugLabel;
 import com.techflow.openfda.drugs.Seriousness;
+import com.techflow.openfda.drug.client.DrugRepositoryImpl;
 
-/*
- * import org.apache.lucene.analysis.standard.StandardAnalyzer;
- * import org.apache.lucene.document.Document;
- * import org.apache.lucene.document.Field;
- * import org.apache.lucene.index.CorruptIndexException;
- * import org.apache.lucene.index.IndexWriter;
- * //import org.apache.lucene.queryParser.MultiFieldQueryParser;
- * import org.apache.lucene.queryParser.QueryParser;
- * import org.apache.lucene.queryParser.ParseException;
- * import org.apache.lucene.search.IndexSearcher;
- * import org.apache.lucene.search.Query;
- * import org.apache.lucene.search.ScoreDoc;
- * import org.apache.lucene.search.TopDocCollector;
- * import org.apache.lucene.store.Directory;
- * import org.apache.lucene.store.FSDirectory;
- * import org.apache.lucene.store.LockObtainFailedException;
- * import org.apache.lucene.store.RAMDirectory;
- */
+
 
 public class OpenFdaGatewayImplTest
 {
@@ -372,16 +357,15 @@ public class OpenFdaGatewayImplTest
 	public void findBrandNames() throws IOException
 	{
 
-		final URL repoUrl = Resources.getResource("brand-names.txt");
-
-		DrugRepositoryImpl drugRepo = null;
-		drugRepo = new DrugRepositoryImpl(repoUrl.getFile());
-
+		DrugRepository drugRepo = new DrugRepositoryImpl();
+		
 		ArrayList<String> drugNames = (ArrayList<String>)drugRepo.startsWith("multi*");
 
-		System.out.println("******** Name: " + drugNames.get(1));
+		System.out.println("******** Name: " + drugNames.get(0));
 
 		// assertThat("true", equalTo("true"));
+		
+		assertThat( drugNames.size(), lessThanOrEqualTo(5) );
 
 		assertThat(drugNames.get(0), equalTo(drugNames.get(0)));
 
